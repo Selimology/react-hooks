730b32df1cb8cf62b3d7948901e2b9184e1217a5
Selimology <kertekin2018@gmail.com>
2022-12-03 05:56:10 +0100

    useState - update state in object

--- src/UseStateExample.js
+++ src/UseStateExample.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
 
 const UseStateExample = () => {
-  const [name, setName] = useState('');
-  const [click, setClick] = useState(0);
+  // const [name, setName] = useState('');
+  // const [click, setClick] = useState(0);
+  const [details, setDetails] = useState({ name: '', click: 0 });
+
+  const changeName = (e) => {
+    setDetails((prev) => ({
+      ...prev,
+      name: e.target.value,
+    }));
+  };
 
   function increaseCounter() {
-    setClick(click + 1);
+    // setClick(click + 1);
+    setDetails((prev) => ({
+      ...prev,
+      click: prev.click + 1,
+    }));
   }
+  console.log(details);
+
   return (
     <>
-      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <input type="text" value={details.name} onChange={changeName} />
       <h1>
-        {name} has clicked times {click}
+        {details.name} has clicked times {details.click}
       </h1>
       <button onClick={increaseCounter}>Increase</button>
     </>
