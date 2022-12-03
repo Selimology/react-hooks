f0d2712e46353974c2ade2a3ba4ccb355cea5a13
Selimology <kertekin2018@gmail.com>
2022-12-03 14:20:55 +0100

    useEffect - array with variables

--- src/UseEffectExample.js
+++ src/UseEffectExample.js
@@ -2,16 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 const UseEffectExample = () => {
   const [count, setCount] = useState(0);
+  const [otherCount, setOtherCount] = useState(5);
 
   //Runs only once if empty array (first time)
   useEffect(() => {
-    document.title = `${count} new messages`;
-  }, []);
+    document.title = `${otherCount} new messages`;
+    console.log('run + 1 ');
+  }, [otherCount]);
 
   return (
     <div>
       <h3>{count} new messages!</h3>
       <button onClick={() => setCount(count + 1)}>Increase</button>
+      <h3>Other Count : {otherCount}</h3>
+      <button onClick={() => setOtherCount(otherCount + 5)}>
+        Increase by 5{' '}
+      </button>
     </div>
   );
 };
